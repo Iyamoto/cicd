64495dd6685e59f41b4a0c9b11fe363df9173ab3
Iyamoto <Iyamoto@users.noreply.github.com>
2017-10-15 14:01:09 +0300

    upload

---
+++ scripts/bootstrap_ansible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Ansible..."
+# sudo yum update -y
+sudo yum install -y epel-release
+sudo yum install -y ansible
+cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
